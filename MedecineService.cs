2171229063f348627562ba5fc1cac7853f34cbe4
Amrish11 <31763931+Amrish11@users.noreply.github.com>
2020-09-21 17:25:09 +0530

    writing and reading file from json file using C# and web Api

---
+++ MedecineService.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace MedProject.Model
+{
+    public class MedecineService
+    {
+        string jsonFilePath = @"C:\Users\Developer\source\repos\MedProject\Med.json";
+        public List<Medecine> GetMedecines()
+        {
+            try
+            {
+                string json = File.ReadAllText(jsonFilePath);
+                var jsonToreturn = JsonConvert.DeserializeObject<List<Medecine>>(json);
+                return jsonToreturn;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public bool AddOrUpdateMedeCine(Medecine medecine)
+        {
+            try
+            {
+                List<Medecine> med = new List<Medecine>();
+                string json = File.ReadAllText(jsonFilePath);
+                med = JsonConvert.DeserializeObject<List<Medecine>>(json).ToList();
+                var medecines = med.Where(a => a.MedecineName == medecine.MedecineName).FirstOrDefault();
+                if (medecines == null)
+                {
+                    med.Add(medecine);
+                }
+                else
+                {
+                    int index = med.FindIndex(x => x.MedecineName == medecine.MedecineName);
+                    med[index] = medecine;
+                }
+
+                string jsonToStore = JsonConvert.SerializeObject(med);
+                System.IO.File.WriteAllText(jsonToStore, jsonToStore);
+                return true;
+            }
+            catch(Exception ex )
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedProject.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MedProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Medecine> Get()
+        {
+            MedecineService md = new MedecineService();
+            return md.GetMedecines();
+        }
+
+        [HttpPost]
+        public bool Post(Medecine med)
+        {
+            MedecineService md = new MedecineService();
+            var result = md.AddOrUpdateMedeCine(med);
+            return result;
+        }
+    }
+}
